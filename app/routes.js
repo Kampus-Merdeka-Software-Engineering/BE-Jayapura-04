5b1fd2b869fbc8c807f00651cce12de6569b3093
JoopJoergen <142917419+JoopJoergen@users.noreply.github.com>
2023-10-03 12:24:06 +0800

    Delete app directory

--- app/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require("./controller/user");
-
-router.get('/register', controller.login);
-router.post('/login', controller.signup);
-
-module.exports = router;
