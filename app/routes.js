31112960672094a90ed6e848f59bb5934702e76a
Gina Ganiyah <ginaganiyah29@gmail.com>
2023-10-03 21:14:00 +0700

    tambahan

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ const controller = require("./controller/kategori");
 const controller = require("./controller/user");
 
 router.get('/kategori', controller.kategori);
-router.get('/register', controller.login);
+router.get('/signup', controller.login);
 router.post('/login', controller.signup);
 
 module.exports = router;
