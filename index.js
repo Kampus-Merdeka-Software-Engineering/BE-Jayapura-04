06d734e472c447914248e38c6cc742dd65751ddf
Gina Ganiyah <ginaganiyah29@gmail.com>
2023-10-09 12:30:12 +0700

    app.listen

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.use(express.urlencoded({ extended: false }));
 // http router
 app.use("/", mainRouter);
 
-const port = 3000;
-app.listen(port, function () {
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function () {
     console.log("Server start on", port);
     connection.authenticate()
         .then(function () {
